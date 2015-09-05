e3c9263712abd212b706c1c9246b5a9b195b68b3
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-04 20:40:34 -0700

    [#102564060] add jasmine-jquery matchers to unit test setup

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
     config.set({
         browsers: ['PhantomJS'],
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine-jquery', 'jasmine'],
         files: [
             'dist/js/app.*.js',
             'node_modules/angular-mocks/angular-mocks.js',
