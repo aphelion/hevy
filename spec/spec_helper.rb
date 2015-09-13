f1ae2b6f04e60d51984d7c8e0de52711ad0cf1e0
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-13 11:47:04 -0500

    [#102809990] add dugway gem to run Big Cartel app.
    - capybara tests now run against dugway (rack app)
    - switch out poltergeist for capybara-webkit because Jenkins was having issues using both the "NodeJS Plugin" and the "rbenv plugin". switching to capybara-webkit allows us to avoid installing phantomjs via npm. https://issues.jenkins-ci.org/browse/JENKINS-24425

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,14 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+require 'dugway'
 
-phantomjs_path = File.join(File.dirname(__FILE__), "../node_modules/.bin/phantomjs")
+#phantomjs_path = File.join(File.dirname(__FILE__), "../node_modules/.bin/phantomjs")
 
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app,
-    {phantomjs: phantomjs_path}
-  )
-end
+#Capybara.register_driver :poltergeist do |app|
+#  Capybara::Poltergeist::Driver.new(app,
+#    {phantomjs: phantomjs_path}
+#  )
+#end
 
-Capybara.default_driver = :poltergeist
-Capybara.app_host = 'http://localhost:8000'
+#Capybara.default_driver = :webkit
+#Capybara.app_host = 'http://localhost:8000'
+Capybara.app = Dugway.application
