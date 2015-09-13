890b85087123955fe10afe83022059345ecccda0
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-13 14:10:04 -0500

    [#102809990] setup capybara to work with webkit
    - removes any dependency on node to run rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,6 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+require 'capybara/webkit'
 require 'dugway'
 
-phantomjs_path = File.join(File.dirname(__FILE__), '../node_modules/.bin/phantomjs')
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, {phantomjs: phantomjs_path})
-end
-
-Capybara.default_driver = :poltergeist
+Capybara.default_driver = :webkit
 Capybara.app = Dugway.application
