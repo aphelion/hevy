6978f30025a5b2359acad1ccd3470655247457fd
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-18 15:08:01 -0400

    [#103693266] include head_content tag in <head>
    - "loads important system code from our server"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'dugway'
 Capybara::Webkit.configure do |config|
   config.allow_url('ajax.googleapis.com')
   config.allow_url('maxcdn.bootstrapcdn.com')
+  config.block_unknown_urls
 end
 
 Capybara.default_driver = :webkit
