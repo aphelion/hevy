5aafe9369b505d2154dd0d3d7fd25dcbaf16ba55
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-18 15:38:00 -0400

    [#103694126] customer can see product images for each product on the products page

--- spec/products_page_spec.rb
+++ spec/products_page_spec.rb
@@ -1,9 +1,26 @@
 describe 'Products Page', type: :feature do
-  it 'lists all the products' do
+  before :each do
     visit '/products'
-    content = find('#content')
+    @content = find('#content')
+  end
 
-    expect(content.find('ul')).to have_selector('li', count: 7)
-    expect(content).to have_content 'DIY or DIE Hoodie'
+  it 'lists all the products' do
+    expect(@content.find('ul')).to have_selector('li', count: 7)
+    expect(@content).to have_content 'DIY or DIE Hoodie'
+  end
+
+  it 'contains an image for each product' do
+    @content.all('ul li').each do |li|
+      expect(li).to have_selector('img')
+    end
+  end
+
+  it 'contains a valid image for each product' do
+    images = @content.all('ul li img').map { |img| img[:src] }
+
+    images.each do |link|
+      visit link
+      expect(page.status_code).to be(200)
+    end
   end
 end
