60c93a9df28dea5525d5fcb887df8affc3953000
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-20 22:34:57 -0400

    [#103694544] customer can can see the product image on a product page

--- spec/features/product_page_spec.rb
+++ spec/features/product_page_spec.rb
@@ -4,7 +4,13 @@ describe 'Product Page', type: :feature do
     @content = find('#content')
   end
 
-  it 'contains the product title' do
+  it 'displays the product title' do
     expect(@content).to have_content 'DIY or DIE Hoodie'
+  end
+
+  it 'displays the primary product image' do
+    expect(@content).to have_selector('img[src]')
+    visit @content.find('img')[:src]
+    expect(page.status_code).to be(200)
   end
 end
