c877a82a2035911e170af51f70247c8eb2590d1b
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-20 22:25:40 -0400

    [#103694544] refactor navigation_spec for DRY

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -1,27 +1,29 @@
 describe 'Site Navigation', type: :feature do
+  before :each do
+    visit '/'
+    @content = find('#content')
+  end
+
   describe 'the layout' do
     it 'has a navbar' do
-      visit '/'
       expect(page).to have_selector '.navbar'
     end
   end
 
   describe 'the PRODUCTS link' do
     it 'takes the customer to the products page' do
-      visit '/'
       find_link('PRODUCTS').click
-      expect(find('#content')).to have_content 'PRODUCTS'
-      expect(find('#content')).not_to have_content 'HOME'
+      expect(@content).to have_content 'PRODUCTS'
+      expect(@content).not_to have_content 'HOME'
     end
   end
 
   describe 'the HOME link' do
     it 'takes the customer to the home page' do
-      visit '/'
       find_link('PRODUCTS').click
       find_link('HOME').click
-      expect(find('#content')).to have_content 'HOME'
-      expect(find('#content')).not_to have_content 'PRODUCTS'
+      expect(@content).to have_content 'HOME'
+      expect(@content).not_to have_content 'PRODUCTS'
     end
   end
 end
